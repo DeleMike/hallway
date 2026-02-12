cc6cd149f3e5d0dd3cd70644d4689a078c378f38
DeleMike <akindelemichael65@gmail.com>
2026-02-12 14:50:13 +0100

    build server initial state

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
-	"sync"
+	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
@@ -13,108 +14,154 @@ const (
 	System      = "system"
 	UserCount   = "userCount"
 	ChatMessage = "chatMessage"
+	DateFormat  = "150405" // Hour, Minute, Second
 )
 
-// a typical message coming from the client
 type Message struct {
 	Type    string `json:"type"`
 	Payload any    `json:"payload"`
 }
 
-// to help us change from http(s) to ws(s)
+type Client struct {
+	conn *websocket.Conn
+	send chan Message
+	name string
+}
+
+type Hub struct {
+	clients    map[*Client]bool
+	broadcast  chan Message
+	register   chan *Client
+	unregister chan *Client
+}
+
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool { return true },
 }
 
-var clients = make(map[*websocket.Conn]string)
-var mutex = &sync.Mutex{}
-
-func chatHandler(c *gin.Context) {
-	// upgrade connection
-	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
-	if err != nil {
-		log.Println("We could not upgrade connection: ", err)
-		return
+func newHub() *Hub {
+	return &Hub{
+		clients:    make(map[*Client]bool),
+		broadcast:  make(chan Message),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
 	}
+}
 
-	// collect user name
-	username := c.Query("username")
-	if username == "" {
-		username = "Anon"
-	}
-
-	mutex.Lock()
-	clients[conn] = username
-	mutex.Unlock()
-
-	// update clients statuses
-	broadcastUserCount()
-	broadcastSystem(username + " joined")
-
-	// will run when a client disconnects
-	defer func() {
-		mutex.Lock()
-		delete(clients, conn)
-		mutex.Unlock()
-
-		broadcastUserCount()
-		broadcastSystem(username + " left")
-
-		conn.Close()
-	}()
-
-	// a pipe that waits for any incoming message
+func (h *Hub) run() {
 	for {
-		var msg Message
+		select {
 
-		if err := conn.ReadJSON(&msg); err != nil {
-			break
-		}
+		case client := <-h.register:
+			h.clients[client] = true
+			go h.broadcastUserCount()
+			go h.broadcastSystem(client.name + " joined")
 
-		if msg.Type == ChatMessage {
-			broadcast(msg)
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+				go h.broadcastUserCount()
+				go h.broadcastSystem(client.name + " left")
+			}
+
+		case message := <-h.broadcast:
+			for client := range h.clients {
+				client.send <- message
+			}
 		}
 	}
-
 }
 
-// broadcast sends message to pipe for all connected clients to receive
-func broadcast(message Message) {
-	mutex.Lock()
-	defer mutex.Unlock()
-
-	for client := range clients {
-		client.WriteJSON(message)
-	}
-}
-
-// broadcastUserCount counts how many connected clients in the room
-func broadcastUserCount() {
+func (h *Hub) broadcastUserCount() {
 	msg := Message{
 		Type: UserCount,
 		Payload: map[string]int{
-			"count": len(clients),
+			"count": len(h.clients),
 		},
 	}
-
-	broadcast(msg)
+	h.broadcast <- msg
 }
 
-// broadcastSystem is used to notify who joined the room
-func broadcastSystem(text string) {
+func (h *Hub) broadcastSystem(text string) {
 	msg := Message{
 		Type: System,
 		Payload: map[string]string{
 			"text": text,
 		},
 	}
+	h.broadcast <- msg
+}
 
-	broadcast(msg)
+func (c *Client) readPump(h *Hub) {
+	defer func() {
+		h.unregister <- c
+		c.conn.Close()
+	}()
+
+	for {
+		var msg Message
+		if err := c.conn.ReadJSON(&msg); err != nil {
+			break
+		}
+
+		if msg.Type == ChatMessage {
+			h.broadcast <- msg
+		}
+	}
+}
+
+func (c *Client) writePump() {
+	defer c.conn.Close()
+
+	for msg := range c.send {
+		if err := c.conn.WriteJSON(msg); err != nil {
+			log.Println("Write error:", err)
+			break
+		}
+	}
+}
+
+func serveChat(hub *Hub, c *gin.Context) {
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		log.Println("Upgrade error:", err)
+		return
+	}
+
+	username := c.Query("username")
+	if username == "" {
+		var sb strings.Builder
+		sb.WriteString("Anon_")
+		sb.WriteString(time.Now().Format(DateFormat))
+		username = sb.String()
+	}
+
+	client := &Client{
+		conn: conn,
+		send: make(chan Message, 256),
+		name: username,
+	}
+
+	hub.register <- client
+
+	go client.writePump()
+	go client.readPump(hub)
 }
 
 func main() {
-	r := gin.Default()
-	r.GET("/chat", chatHandler)
-	r.Run(":8080")
+	hub := newHub()
+	go hub.run()
 
+	r := gin.Default()
+
+	// Serve the frontend
+	r.StaticFile("/", "./static/index.html")
+
+	r.GET("/chat", func(c *gin.Context) {
+		serveChat(hub, c)
+	})
+
+	log.Println("Hallway running on :8080")
+	r.Run(":8080")
 }
